a5c4c4f954abf37cf4b2717415225b07a4888693
mmahanth <93912665+mmahanth@users.noreply.github.com>
2023-12-08 11:03:42 +0530

    Update first lab program on weather modelling.py

--- first lab program on weather modelling.py
+++ first lab program on weather modelling.py
@@ -1,22 +1,32 @@
-def temperature_modeling(a, b, c, time):
-    # Calculate temperature based on time using the quadratic equation
-    temperature = a * time**2 + b * time + c
-    return temperature
-a_hardcoded, b_hardcoded, c_hardcoded = 0.1, 2, 10
-print("Step 1: Hard-coded Variables for Weather Modeling")
-time_hardcoded = 5
-print("Temperature for hardcoded coefficients at time", time_hardcoded, "hours:", temperature_modeling(a_hardcoded, b_hardcoded, c_hardcoded, time_hardcoded))
-print("\n")
-a_keyboard = float(input("Enter coefficient a: "))
-b_keyboard = float(input("Enter coefficient b: "))
-c_keyboard = float(input("Enter coefficient c: "))
-time_keyboard = float(input("Enter time in hours: "))
-print("Step 2: Keyboard Input for Weather Modeling")
-print("Temperature for keyboard input coefficients at time", time_keyboard, "hours:", temperature_modeling(a_keyboard, b_keyboard, c_keyboard, time_keyboard))
-print("\n")
-with open('weather.txt', 'r') as file:
-    lines = file.readlines()
-    a_file, b_file, c_file, time_file = map(float, lines[0].split())
-print("Step 3: Read from a File for Weather Modeling")
-print("Temperature for file input coefficients at time", time_file, "hours:", temperature_modeling(a_file, b_file, c_file, time_file))
-print("\n")
+def quadratic_solver(a, b, c):
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        root1 = (-b + discriminant**0.5) / (2*a)
+        root2 = (-b - discriminant**0.5) / (2*a)
+        return root1, root2
+    elif discriminant == 0:
+        root = -b / (2*a)
+        return root,
+    else:
+        return "No real roots"
+a = 1
+b = -3
+c = 2
+roots = quadratic_solver(a, b, c)
+print("Roots:", roots)
+def get_coefficients():
+    a = float(input("Enter coefficient a: "))
+    b = float(input("Enter coefficient b: "))
+    c = float(input("Enter coefficient c: "))
+    return a, b, c
+a, b, c = get_coefficients()
+roots = quadratic_solver(a, b, c)
+print("Roots:", roots)
+def get_coefficients():
+    a = float(input("Enter coefficient a: "))
+    b = float(input("Enter coefficient b: "))
+    c = float(input("Enter coefficient c: "))
+    return a, b, c
+a, b, c = get_coefficients()
+roots = quadratic_solver(a, b, c)
+print("Roots:", roots)
